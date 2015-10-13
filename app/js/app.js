4dc96f2c699afc5969e622792f91db14456aba8c
endou_cai <endou_cai@sinobasedm.com>
2015-10-13 12:12:18 +0800

    规范结构

---
+++ app/js/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.Animations',
+  'myApp.Controllers',
+  'myApp.Filters'
+]).
+config(['$routeProvider', function($routeProvider) {
+      $routeProvider.
+          when("/view1",{
+            templateUrl: 'partials/view1.html',
+            controller: 'View1Ctrl'}).
+          when("/view2",{
+            templateUrl: 'partials/view2.html',
+            controller: 'View2Ctrl'})
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
---
+++ app/js/controllers.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/* Controllers */
+
+var phonecatControllers = angular.module('myApp.Controllers', []);
+
+phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
+  function($scope, Phone) {
+    $scope.phones = Phone.query();
+    $scope.orderProp = 'age';
+  }]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
+  function($scope, $routeParams, Phone) {
+    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+      $scope.mainImageUrl = phone.images[0];
+    });
+
+    $scope.setImage = function(imageUrl) {
+      $scope.mainImageUrl = imageUrl;
+    };
+  }]);
+
+phonecatControllers.controller('View1Ctrl', [function() {
+
+}]);
+
+phonecatControllers.controller('View2Ctrl', [function() {
+
+}]);
